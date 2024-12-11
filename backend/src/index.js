e638923d6ecb02236a8a34c8a9464a1db010c20f
nafisdmk <157614573+nafisdmk@users.noreply.github.com>
2024-12-12 01:31:28 +0530

    utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 console.log("hello")
 import connnectDB from "./db/index.js"
 import dotenv from "dotenv"
+import { app } from "./app.js"
 
 dotenv.config()
 
 connnectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed",err);
+})
